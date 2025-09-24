1f0edd1d10272b7aa0f60b492a34f5cee2ebfaa1
CarlosMR19 <escanorbest7@gmail.com>
2025-09-23 20:32:13 -0500

    creacion de la app motos y creacion del modelo motos

---
+++ concesionario/motos/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Motos(models.Model):
+    name = models.Charfield(max_lenght=100)
+    descripcion = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
